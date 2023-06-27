b01780ddcacb047a4fdb12c637c7f04e35f3439a
TomOwen93 <mr.tsowen@gmail.com>
2023-06-27 17:58:32 +0100

    Add MainContent, PageHeader, PlaceItem and Places

---
+++ src/components/PlaceItem.tsx
@@ -0,0 +1,30 @@
+interface PlaceProps {
+  title: string;
+  placeName: string;
+  countryName: string;
+  mainImage?: string;
+  mapLocation?: string;
+  explanation: string;
+}
+
+function PlaceItem({
+  title,
+  placeName,
+  countryName,
+  mainImage,
+  mapLocation,
+  explanation,
+}: PlaceProps): JSX.Element {
+  return (
+    <div className="placeStyle">
+      <h1 className="placeTitle">{title}</h1>
+      <h2 className="countryTitle">{placeName} - {countryName}</h2>
+      <img className="placeImage" src={mainImage}></img>
+      <p>
+        <strong>Reason:</strong> {explanation}
+      </p>
+    </div>
+  );
+}
+
+export default PlaceItem;
